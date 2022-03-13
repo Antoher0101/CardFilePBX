f215d8f45d04065c9404d69706f9793a1afc748b
Antoher0101 <firelessball@yandex.ru>
2022-03-13 22:40:22 +0500

    Changed the encoding of some fields in the database.

--- DatabaseApplication.cs
+++ DatabaseApplication.cs
@@ -13,11 +13,12 @@ namespace CardFilePBX
     {
         public SqlConnection connection { get; set; }
         public DataView dataView { get; set; }
-        private DataTable table = new DataTable();
+        private DataTable table;
         public ConnectionState State { get; set; }
         public DatabaseApplication()
         {
             connection = new SqlConnection(ConfigurationManager.ConnectionStrings["AbonentsDB"].ConnectionString);
+            table = new DataTable();
         }
         public void Open()
         {
@@ -25,7 +26,6 @@ namespace CardFilePBX
             {
                 connection.Open();
                 State = ConnectionState.Open;
-                Console.WriteLine("База данных успешно подключена.");
             }
             catch
             {
@@ -47,26 +47,17 @@ namespace CardFilePBX
             adapter.Fill(table);
 
             dataView = table.DefaultView;
-        }
+		}
         public void AddAbonent(int id, string first_name, string last_name, string patronymic, string phone_number)
         {
             string query = $"INSERT INTO Abonents (first_name, last_name, patronymic, phone_number) VALUES (@first_name, @last_name, @patronymic, @phone_number)";
             using (SqlCommand command = new SqlCommand(query, connection))
             {
-                command.Parameters.Add("@first_name", SqlDbType.Text, 16).Value = first_name;
-                command.Parameters.Add("@last_name", SqlDbType.Text, 16).Value = last_name;
-                command.Parameters.Add("@patronymic", SqlDbType.Text, 16).Value = patronymic;
+                command.Parameters.Add("@first_name", SqlDbType.NVarChar, 16).Value = first_name;
+                command.Parameters.Add("@last_name", SqlDbType.NVarChar, 16).Value = last_name;
+                command.Parameters.Add("@patronymic", SqlDbType.NVarChar, 16).Value = patronymic;
                 command.Parameters.Add("@phone_number", SqlDbType.Text, 16).Value = phone_number;
 
-                command.ExecuteNonQuery();
-            }
-        }
-
-        public void RandQuery()
-        {
-            string query = $"INSERT T(F) VALUES(RAND()); GO 10000";
-            using (SqlCommand command = new SqlCommand(query, connection))
-            {
                 command.ExecuteNonQuery();
             }
         }
@@ -83,7 +74,6 @@ namespace CardFilePBX
             command.ExecuteNonQuery();
         }
     }
-
     public class DbStateToBoolean : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -91,7 +81,6 @@ namespace CardFilePBX
             if (((ConnectionState)value) == ConnectionState.Open)
                 return true;
             return false;
-
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
